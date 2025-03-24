d375ec658b6f55e024914ffd653dfa6b8577d80a
Matheusify <108300707+Matheusify@users.noreply.github.com>
2025-03-23 22:50:41 -0300

    Update live.vs.js

--- assets/js/live.vs.js
+++ assets/js/live.vs.js
@@ -2,30 +2,11 @@ YT.live = {
   vs1: "",
   vs2: "",
   update: function () {
-    $.getJSON("https://axern.space/api/get?platform=youtube&type=channel&id=" + YT.live.vs1 + "", function (f) {
-      $.getJSON("https://axern.space/api/get?platform=youtube&type=channel&id=" + YT.live.vs2 + "", function (g) {
-        $.getJSON("https://corsproxy.io/?https://studio.nia-statistics.com/api/channel/" + YT.live.vs1, function (e) {
-          $.getJSON("https://corsproxy.io/?https://studio.nia-statistics.com/api/channel/" + YT.live.vs2, function (h) {
+    $.getJSON("/api/youtube-subcount/" + YT.live.vs1 + "", function (f) {
+      $.getJSON("/api/youtube-subcount/" + YT.live.vs2 + "", function (g) {
         YT.updateManager.updateChannelI1D(encodeURIComponent(YT.live.vs1));
         YT.updateManager.updateChannelID2(encodeURIComponent(YT.live.vs2));
-        if (e.success == false) {
-        YT.updateManager.updateSubscribers(f.estSubCount, g.estSubCount);
-        }
-         else if (h.success == false) {
-          YT.updateManager.updateSubscribers(e.channels.counts[2].count, g.estSubCount);
-          let textyab = document.getElementById("yt_substext1").innerHTML;
-          let idkyab = textyab.replace(/Estimated/gi, "Studio")
-          document.getElementById("yt_substext1").textContent = idkyab
-         }
-          else {
-            YT.updateManager.updateSubscribers(e.channels.counts[2].count, h.channels.counts[2].count);
-            let textyab = document.getElementById("yt_substext1").innerHTML;
-            let idkyab = textyab.replace(/Estimated/gi, "Studio")
-            document.getElementById("yt_substext1").textContent = idkyab
-            let textya1b = document.getElementById("yt_substext2").innerHTML;
-            let idkya1b = textya1b.replace(/Estimated/gi, "Studio")
-            document.getElementById("yt_substext2").textContent = idkya1b
-          }
+        YT.updateManager.updateSubscribers(f.stats.subCount, g.stats.subCount);
       });
     });
   });
