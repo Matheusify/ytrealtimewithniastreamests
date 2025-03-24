11715fa2f04eb8fd553a6cdba2a5f639cd5a901b
Matheusify <108300707+Matheusify@users.noreply.github.com>
2025-03-23 23:33:54 -0300

    Update live.vs.js

--- assets/js/live.vs.js
+++ assets/js/live.vs.js
@@ -2,11 +2,11 @@ YT.live = {
   vs1: "",
   vs2: "",
   update: function () {
-    $.getJSON("/api/youtube-subcount/" + YT.live.vs1, function (e) {
-      $.getJSON("/api/youtube-subcount/" + YT.live.vs2, function (f) {
+    $.getJSON("/api/youtube-subcount/" + YT.live.vs1, function (g) {
+      $.getJSON("/api/youtube-subcount/" + YT.live.vs2, function (h) {
         YT.updateManager.updateChannelI1D(encodeURIComponent(YT.live.vs1));
         YT.updateManager.updateChannelID2(encodeURIComponent(YT.live.vs2));
-        YT.updateManager.updateSubscribers(e.stats.subCount, f.stats.subCount);
+        YT.updateManager.updateSubscribers(g.stats.subCount, h.stats.subCount);
       });
     });
   });
